9c06a6d1891236fe1fc4b56ced63965cbc163c43
AJ Rossini <blindglobe@gmail.com>
2009-12-20 14:10:00 +0100

    fake lists-of-same-size function -- need a real version (see doc string for a start).
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- src/listoflist.lisp
+++ src/listoflist.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-12-12 08:04:47 tony>
+;;; Time-stamp: <2009-12-20 12:52:06 tony>
 ;;; Creation:   <2008-09-08 08:06:30 tony>
 ;;; File:       listoflist.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -72,7 +72,16 @@ return T/nil based on equality."
        ;; (loop over x and verify same tree as y)
        ))
 
+(defun lists-of-same-size (args)
+  "WRITEME!
+ (reduce #'(lambda (x y)
+	      (if (= x y) y -1))
+	  (mapcar #'length (list *x1* *x2* *x3*)))
+ (reduce #'(lambda (x y)
+	      (if (= x y) y -1))  (list 2 3 2))
 
+"
+  nil)
 
 #|
   (defparameter *mdfl-test*
